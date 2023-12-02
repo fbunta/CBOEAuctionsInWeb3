47a8c6927796960db4a6019ba38bb4ee87d9972e
Finnian Bunta <fbunta@belvederetrading.com>
2023-12-02 17:40:06 -0600

    Rounded out starting auction logic
    auction start should only be done by two
    order where one is auction only and the other
    is auction only/elgible

--- src/contracts/Auction.sol
+++ src/contracts/Auction.sol
@@ -7,6 +7,8 @@ contract CBOEPeriodicAuction {
     // By default initialized to `false`.
     bool public is_auction_live;
 
+    uint256 public auction_start_time;
+
     Order[] private buy_arr;
     Order[] private sell_arr;
 
@@ -34,13 +36,18 @@ contract CBOEPeriodicAuction {
     }
     
     struct Order {
+        address bidder;
         OrderType order_type;
         Side side;
         uint qty;
         uint256 price;
     }
 
-    function OrderFactory(uint order_type_int, uint coin_int, uint bid_qty, uint offered_qty) private pure returns (Order memory) {
+    function OrderFactory(address bidder, uint order_type_int, uint coin_int, uint bid_qty, uint offered_qty)
+            private
+            pure
+            returns (Order memory) 
+    {
         OrderType order_type;
         if (order_type_int == orderTypeEnumToInt(OrderType.Normal)) {
             order_type = OrderType.Normal;
@@ -61,6 +68,7 @@ contract CBOEPeriodicAuction {
         }
         (uint price, uint qty, Side side) = get_order_attr(coin, bid_qty, offered_qty);
         Order memory order;
+        order.bidder = bidder;
         order.order_type = order_type;
         order.side = side;
         order.qty = qty;
@@ -70,6 +78,8 @@ contract CBOEPeriodicAuction {
 
     constructor() {
         exchange_admin = msg.sender;
+        auction_id = 0;
+        is_auction_live = false;
     }
 
     modifier onlyAdmin() {
@@ -93,35 +103,72 @@ contract CBOEPeriodicAuction {
 
     // TODO make this payable and get rid of offered qty
     function place_bid(address bidder, uint order_type_int, uint coin_int, uint bid_qty, uint offered_qty) public {
-        Order memory ord = OrderFactory(order_type_int, coin_int, bid_qty, offered_qty);
+        Order memory ord = OrderFactory(bidder, order_type_int, coin_int, bid_qty, offered_qty);
+        if(!is_auction_live) {
+            if (ord.order_type == OrderType.AuctionOnly || ord.order_type == OrderType.AuctionEligible) {
+                if(is_auction_start(ord)){
+                    add_order_to_orderbook(ord);
+                    start_auction();
+                } else {
+                    add_order_to_orderbook(ord);
+                }
+            } else {
+                add_order_to_orderbook(ord);
+            }
+        } // TODO add else logic for handling orders when an auction already live
+        return;
+    }
+
+    function add_order_to_orderbook(Order memory ord) private {
         if (ord.side == Side.Sell) {
             sell_arr.push(ord);
         } else {
             buy_arr.push(ord);
         }
-        if(!is_auction_live){
-            check_for_auction_start();
-        }
-        return;
     }
 
-    function set_auction_time_random() private pure {
-        // randomness https://docs.chain.link/vrf/v2/best-practices
-        return;
+    // TODO figure out a way this is not manually called
+    function manually_end_auction() public onlyAdmin() {
+        require(block.timestamp >= auction_start_time + set_auction_time_random(), "Not enough time has passed");
+        end_auction();
     }
 
-    function check_for_auction_start() private view {
-        for (uint256 i = 0; i < sell_arr.length; i++) {
-            for (uint256 j = 0; j < buy_arr.length; j++) {
-                if (buy_arr[j].price == sell_arr[i].price) {
-                    start_auction();
-                    break;
+    function set_auction_time_random() private pure returns(uint256) {
+        // TODO(Neal) add randomness https://docs.chain.link/vrf/v2/best-practices
+        return 65;
+    }
+
+    function is_one_order_auction_only(Order memory ord_1, Order memory ord_2) private pure returns (bool) {
+        return (ord_1.order_type == OrderType.AuctionOnly || ord_2.order_type == OrderType.AuctionOnly);
+    }
+
+    function is_auction_start(Order memory ord) private view returns (bool) {
+        if (ord.side == Side.Sell) {
+            for (uint256 i = 0; i < buy_arr.length; i++) {
+                if (buy_arr[i].price == ord.price && is_one_order_auction_only(ord, buy_arr[i])) {
+                    return true;
                 }
-            }   
+            }  
+            return false;
+        } else if (ord.side == Side.Buy) {
+            for (uint256 i = 0; i < sell_arr.length; i++) {
+                if (sell_arr[i].price == ord.price && is_one_order_auction_only(ord, sell_arr[i])) {
+                    return true;
+                }
+            }  
+            return false;
         }
+        return false;
     }
 
-    function start_auction() private pure {
-        return;
+    function start_auction() private {
+        auction_start_time = block.timestamp;
+        auction_id = auction_id += 1;
+        is_auction_live = true;
+    }
+
+    function end_auction() private {
+        // TODO end the auction execution logic
+        is_auction_live = false;
     }
 }
